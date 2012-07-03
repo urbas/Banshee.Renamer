c025789506894b13e60e24e17b11c365f169e79c
Matej Urbas <matej.urbas@gmail.com>
2012-07-03 11:24:09 +0100

    - Added autotools support for building and testing the renamer extension.

---
+++ Banshee.Renamer/Tests/TrackInfoParameterMapTest.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace Banshee.Renamer
+{
+	[TestFixture()]
+	public class TrackInfoParameterMapTest
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
